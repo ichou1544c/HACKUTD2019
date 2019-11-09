419f9e6a0b47047b80ff2b69a94c72009451e782
Dien K Tran <dkt180000@utdallas.edu>
2019-11-09 15:03:40 -0600

    react server template

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+      <head>
+        <title>
+          HackUTD 2019 - Fannie Mae Game
+        </title>
+      </head>
+      <body>
+        <div>
+          <h1 class="header">Hey!</h1>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
